c3538daf407fe6276741de64d52d7cf3501cfd81
Rafael Ferreira <andersonrafaelferreira@gmail.com>
2020-02-10 15:40:06 -0300

    PORT

--- index.js
+++ index.js
@@ -1,48 +1,45 @@
-var express = require("express")
-var nunjucks = require("nunjucks")
+var express = require("express");
+var nunjucks = require("nunjucks");
 
 var app = express();
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const users = []
+const users = [];
 
-app.post('/geo', (req, res) => {
- const data = req.body;
+app.post("/geo", (req, res) => {
+  const data = req.body;
 
- users.push(data)
- console.log(users);
+  users.push(data);
+  console.log(users);
 
-//   return res.json(data);
-    return res.redirect('/');
-})
-
-nunjucks.configure('views', {
-    autoescape: true,
-    express: app,
-    watch: true
+  //   return res.json(data);
+  return res.redirect("/");
 });
 
-app.set("view engine", "njk")
-
-app.get('/', function(req, res) {
-   res.render('list', {
-       users
-   })
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+  watch: true
 });
 
-app.get('/new', function(req, res) {
-   res.render('new');
+app.set("view engine", "njk");
+
+app.get("/", function(req, res) {
+  res.render("list", {
+    users
+  });
 });
 
-app.post('/create', function(req, res) {
-
-    users.push(req.body.user)
-
-   return res.redirect('/');
+app.get("/new", function(req, res) {
+  res.render("new");
 });
 
+app.post("/create", function(req, res) {
+  users.push(req.body.user);
 
+  return res.redirect("/");
+});
 
-app.listen(3000)
+app.listen(process.env.PORT);
