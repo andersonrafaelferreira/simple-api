f5b24b9a46d5e208c832a88e81564980823ee6e9
Rafael Ferreira <andersonrafaelferreira@gmail.com>
2020-11-27 14:59:26 -0300

    feat: + body req

--- index.js
+++ index.js
@@ -77,10 +77,12 @@ app.post("/app", function(req, res) {
 
 app.post("/demora", async function(req, res) {
 
+  const details = req.body;
+
   const result = await new Promise((resolve) => setTimeout(resolve, 5000)); 
 
 
-  return res.send({ demora: "5 segundos", result });
+  return res.send({ demora: "5 segundos", result, details });
 });
 
 // production;
